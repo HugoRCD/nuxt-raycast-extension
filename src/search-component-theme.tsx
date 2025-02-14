1147dcd53a873b2208e8738e213741e09eda8ef5
HugoRCD <hugo.richard@epitech.eu>
2025-02-14 11:30:35 +0100

    fix page pro component search

--- src/search-component-theme.tsx
+++ src/search-component-theme.tsx
@@ -1,5 +1,5 @@
 import { showToast, Toast, LaunchProps, getPreferenceValues, open, getSelectedText } from "@raycast/api";
-import { kebabCase } from "scule";
+import { kebabCase, camelCase } from "scule";
 import { components, proComponents } from "./components";
 
 interface Preferences {
@@ -13,6 +13,19 @@ function sanitizeComponentName(componentName: string, prefix: string) {
   return kebabCase(sanitized);
 }
 
+function findComponent(sanitizedName: string): { exists: boolean; isPro: boolean } {
+  const camelCaseName = camelCase(sanitizedName);
+
+  const componentExists = components.includes(camelCaseName);
+
+  const proComponentExists = proComponents.includes(camelCaseName);
+
+  return {
+    exists: componentExists || proComponentExists,
+    isPro: proComponentExists
+  };
+}
+
 export default async function SearchComponentTheme(props: LaunchProps<{ arguments: Arguments.SearchComponentTheme }>) {
   const { prefix, version } = getPreferenceValues<Preferences>();
   const name = props.arguments?.componentName ?? (await getSelectedText());
@@ -21,15 +34,18 @@ export default async function SearchComponentTheme(props: LaunchProps<{ argument
     await showToast(Toast.Style.Failure, "Please select a component name");
     return;
   }
+
   const sanitizedName = sanitizeComponentName(name, prefix);
-  if (!components.includes(sanitizedName) && !proComponents.includes(sanitizedName)) {
+  const { exists, isPro } = findComponent(sanitizedName);
+
+  if (!exists) {
     await showToast(Toast.Style.Failure, "Component not found");
     return;
   }
 
   let versionUrl = version === "v3" ? "https://ui3.nuxt.dev" : "https://ui.nuxt.com";
 
-  if (proComponents.includes(sanitizedName) && version === "v2") {
+  if (isPro && version === "v2") {
     versionUrl = `${versionUrl}/pro`;
   }
 
